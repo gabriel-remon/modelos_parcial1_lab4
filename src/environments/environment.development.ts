a6326316dda86b594487d1bca04b8a721671a082
gabriel-remon <remongabi16@gmail.com>
2024-05-30 23:23:54 -0300

    primer commit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,4 @@
 export const environment = {
-    firebase:{
-        "projectId":"parcial1-lab4",
-        "appId":"1:94249961215:web:f9c5fffbd22dda70f32eca",
-        "storageBucket":"parcial1-lab4.appspot.com",
-        "apiKey":"AIzaSyAVdDalvT9LsIhj2seNDcaTlrVZsrGTzoc",
-        "authDomain":"parcial1-lab4.firebaseapp.com",
-        "messagingSenderId":"94249961215"}
+    firebase:{} //agregar configuracion firebase
 
 };
